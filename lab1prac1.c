6e8e0c997ffdedc33d49f902c54d7b057590310d
Md Al Amin Hossain <140754672+mdalaminhossain2108051@users.noreply.github.com>
2023-08-12 23:24:58 +0600

    Add files via upload

---
+++ lab1prac1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+    printf("Name:Md Al Amin Hossain\n");
+    printf("Date of birth:02-08-2001\n");
+    printf("Father's Name:Md Shafikul Islum\n");
+    printf("Mother's Name:Most Rasheda Begum\n");
+    printf("Address:Gangachara,Rangpur");
+}
