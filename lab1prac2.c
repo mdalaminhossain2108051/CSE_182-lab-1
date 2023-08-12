5d1ea0a56dc6b216224299a30dc4fe8681f1da0b
Md Al Amin Hossain <140754672+mdalaminhossain2108051@users.noreply.github.com>
2023-08-12 23:25:55 +0600

    Add files via upload

---
+++ lab1prac2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int r;
+    float pi,area;
+    r=8;
+    pi=3.1416;
+    area=pi*r*r;
+    printf("The area of circle with radius %d unit is\n",r);
+    printf("%f square unit",area);
+
+
+
+
+}
