133f8b0e96c8b8f55ad53a2542b290aa6a54b947
Md Al Amin Hossain <140754672+mdalaminhossain2108051@users.noreply.github.com>
2023-08-12 23:26:52 +0600

    Add files via upload

---
+++ lab1prac4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+
+   float a,b,p,area;
+   a=5;
+   b=8;
+   p=2*(a+b);
+   area=a*b;
+   printf("The perimeter of rectangle=%f unit\n",p);
+   printf("The area of rectangle=%f square unit\n",area);
+
+
+}
